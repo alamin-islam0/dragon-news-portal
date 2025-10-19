71412c8ada33d4a39e193c6acbf15079f9018e58
Alamin Islam <alamin648890@gmail.com>
2025-10-19 21:30:20 +0600

    header component added

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
+import LatestNews from '../components/LatestNews';
 
 const HomeLayouts = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto my-4'>
+                    <LatestNews></LatestNews>
+                </section>
             </header>
             <main>
                 <section className='left_nav'></section>
