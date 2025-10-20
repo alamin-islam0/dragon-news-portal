0d6426365d3ef57ea600c84f7102f303ab6a0316
Alamin Islam <alamin648890@gmail.com>
2025-10-20 13:28:29 +0600

    Add Q-Zone component with images layout

---
+++ src/components/HomeLayout/Qzone.jsx
@@ -0,0 +1,14 @@
+const Qzone = () => {
+    return (
+        <div className="p-4 bg-[#F3F3F3]">
+            <h2 className="text-[20px] font-semibold mb-5">Q-Zone</h2>
+            <div className="space-y-5">
+                <img src="/class.png" alt="Class" className="w-full" />
+                <img src="/swimming.png" alt="Swimming" className="w-full" />
+                <img src="/playground.png" alt="Playground" className="w-full" />
+            </div>
+        </div>
+    );
+};
+
+export default Qzone;
